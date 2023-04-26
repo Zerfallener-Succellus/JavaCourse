d353c4d5ca6e9d912a1c6567f9f12cd6fe0f0107
Lucas Godoy <pedrolucf@gmail.com>
2023-04-26 15:05:36 -0300

    Atualizações para bom funcionamento

---
+++ src/main/java/org/example/variables/Main.java
@@ -0,0 +1,28 @@
+package org.example.variables;
+
+public class Main {
+
+    // Curso de Java
+
+
+
+        public static class Variaveis{
+            int number = 6;
+            long population = 12000000000000000L;
+            String text = "hello";
+            char letter = 'a';
+            double decimal = 5.5;
+
+            // A diferença de int e long é a memória utilizada em cada sendop o long maior
+            //o nome das variaveis tem de ser escrito em lowerCamelCase
+
+        }
+
+        public static void main(String[] args) {
+
+            int passageiros = 5;
+            System.out.println(passageiros);
+        }
+
+
+}
