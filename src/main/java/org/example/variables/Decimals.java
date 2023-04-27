992d257cc0ef042d45363ddf9c730237c3347603
Lucas Godoy <pedrolucf@gmail.com>
2023-04-27 14:42:33 -0300

    exercicios com Scanner e operações

--- src/main/java/org/example/variables/Decimals.java
+++ src/main/java/org/example/variables/Decimals.java
@@ -1,2 +1,15 @@
-package org.example.variables;public class Decimals {
+package org.example.variables;
+
+//Double é sempre preferivel para contas matematicas
+//e utilizada para decimais
+public class Decimals {
+    public static void main(String[] args) {
+        double percentage = 73.5;
+        System.out.println(percentage + " of percentages are made up");
+
+        double wallet = 20;
+        double people = 3;
+
+        System.out.println(wallet / people);
+    }
 }
---
+++ src/main/java/org/example/variables/Survey.java
@@ -0,0 +1,44 @@
+package org.example.variables;
+
+import java.util.Scanner;
+
+public class Survey {
+    public static void main(String[] args) {
+
+        //*********PART A: PICKING UP THE USER'S ANSWERS*********
+        System.out.println("Welcome. Thank you for taking the survey");
+
+        Scanner sc = new Scanner(System.in);
+int counter = 0;
+
+        System.out.println("What is your name?");
+        String name = sc.nextLine();
+        counter++;
+
+        System.out.println("How much money do you spend on coffee?");
+        double coffeePrice = sc.nextDouble();
+        counter++;
+
+        System.out.println("How much money do you spend on fast food?");
+        double foodPrice = sc.nextDouble();
+        counter++;
+
+        System.out.println("How many times a week do you buy coffee?");
+        int coffeeAmount = sc.nextInt();
+        counter++;
+
+        System.out.println("How many times a week do you buy fast food?");
+        int foodAmount = sc.nextInt();
+        counter++;
+
+        double totalPriceCoffee = coffeePrice * coffeeAmount;
+        double totalPriceFast = foodPrice * foodAmount;
+
+        sc.close();
+        //*********PART B: RESPONDING TO THE USER**********
+        System.out.println("Thank you "+name+" for answering all "+counter+" questions");
+        System.out.println("Weekly, you spend "+totalPriceCoffee+" on coffee");
+        System.out.println("Weekly, you spend "+totalPriceFast+" on food");
+
+    }
+}
